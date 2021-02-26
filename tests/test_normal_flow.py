8ba71fba3c5ee6ee8cd2732574fe7aaa94a5cd58
Joshua Haertel <josh.haertel@gmail.com>
2021-02-26 01:40:26 -0700

    Break test up into smaller methods

--- tests/test_normal_flow.py
+++ tests/test_normal_flow.py
@@ -8,6 +8,7 @@ from unittest import TestCase
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities, ActionChains
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.wait import WebDriverWait
 
 LOGGER = logging.getLogger(__name__)
@@ -158,6 +159,43 @@ class TestNormalFlow(TestCase):
     ) -> None:
         image_holder = driver.find_element_by_id('id_image_holder')
 
+        self._test_zoom(driver, image_holder)
+
+        settings_elem = driver.find_element_by_id('id_extra_image_settings')
+        expand_settings_elem = driver.find_element_by_id('id_expand_settings')
+        self._test_expand_settings(expand_settings_elem, settings_elem)
+
+        self._test_image_transform(
+            driver,
+            image_holder,
+            left_transform_matrices,
+            right_transform_matrices,
+        )
+
+        image_view_port_elem = driver.find_element_by_id(
+            'id_image_view_port',
+        )
+
+        self._test_image_filters(
+            driver,
+            expand_settings_elem,
+            image_view_port_elem,
+        )
+
+        self._test_thumbnails(driver)
+        json_coverage = driver.execute_script(
+            'return JSON.stringify(window.__coverage__);',
+        )
+        with open(
+                f'/opt/friendexing/tests/coverage_{coverage_name}.json', 'w'
+        ) as json_file:
+            json_file.write(json_coverage)
+
+    def _test_zoom(
+            self,
+            driver: WebDriver,
+            image_holder: WebElement,
+    ) -> None:
         zoom_in_elem = driver.find_element_by_id('id_zoom_in')
         zoom_in_transforms = (
             'matrix(1.5, 0, 0, 1.5, 0, 0)',
@@ -207,13 +245,22 @@ class TestNormalFlow(TestCase):
         transform = image_holder.value_of_css_property('transform')
         self.assertEqual('matrix(1, 0, 0, 1, 0, 0)', transform)
 
-        settings_elem = driver.find_element_by_id('id_extra_image_settings')
+    def _test_expand_settings(
+            self,
+            expand_settings_elem: WebElement,
+            settings_elem: WebElement,
+    ) -> None:
         self.assertFalse(settings_elem.is_displayed())
-
-        expand_settings_elem = driver.find_element_by_id('id_expand_settings')
         expand_settings_elem.click()
         self.assertTrue(settings_elem.is_displayed())
 
+    def _test_image_transform(
+            self,
+            driver: WebDriver,
+            image_holder: WebElement,
+            left_transform_matrices: List[Tuple[str, str]],
+            right_transform_matrices: List[Tuple[str, str]],
+    ) -> None:
         main_image_container = driver.find_element_by_id(
             'id_main_image_container',
         )
@@ -269,20 +316,13 @@ class TestNormalFlow(TestCase):
             self.assertEqual(translation_matrix, transform)
         rotate_right_elem.click()
 
-        image_view_port_element = driver.find_element_by_id(
-            'id_image_view_port',
-        )
-
-        invert_button = driver.find_element_by_id('id_invert_button')
-        invert_button.click()
-
-        filter_style = image_view_port_element.value_of_css_property('filter')
-        self.assertEqual('invert(1) contrast(1) brightness(1)', filter_style)
-
-        invert_button.click()
-
-        filter_style = image_view_port_element.value_of_css_property('filter')
-        self.assertEqual('invert(0) contrast(1) brightness(1)', filter_style)
+    def _test_image_filters(
+            self,
+            driver: WebDriver,
+            expand_settings_elem: WebElement,
+            image_view_port_elem: WebElement,
+    ) -> None:
+        self._test_invert(driver, image_view_port_elem)
 
         tone_modal = driver.find_element_by_id('id_tone_modal')
         self.assertFalse(tone_modal.is_displayed())
@@ -292,60 +332,22 @@ class TestNormalFlow(TestCase):
         self.assertTrue(tone_modal.is_displayed())
 
         brightness_slider = driver.find_element_by_id('id_brightness_slider')
-        brightness_slider_size = brightness_slider.size
-        move_sliders = ActionChains(driver)
-        move_sliders.move_to_element_with_offset(
-            to_element=brightness_slider,
-            xoffset=brightness_slider_size['width'] - 1,
-            yoffset=brightness_slider_size['height'] / 2,
-        )
-        move_sliders.click()
-
         contrast_slider = driver.find_element_by_id('id_contrast_slider')
-        contrast_slider_size = contrast_slider.size
-        move_sliders.move_to_element_with_offset(
-            to_element=contrast_slider,
-            xoffset=contrast_slider_size['width'] - 1,
-            yoffset=contrast_slider_size['height'] / 2,
+        self._test_apply_modal(
+            brightness_slider,
+            contrast_slider,
+            driver,
+            image_view_port_elem,
         )
-        move_sliders.click()
-        move_sliders.perform()
-
-        apply_button = driver.find_element_by_id(
-            'id_apply_image_editor_button',
-        )
-        apply_button.click()
-
-        filter_style = image_view_port_element.value_of_css_property('filter')
-        self.assertEqual('invert(0) contrast(2) brightness(2)', filter_style)
 
         tone_button.click()
 
-        move_sliders.move_to_element_with_offset(
-            to_element=brightness_slider,
-            xoffset=1,
-            yoffset=brightness_slider_size['height'] / 2,
+        self._test_cancel_modal(
+            brightness_slider,
+            contrast_slider,
+            driver,
+            image_view_port_elem,
         )
-        move_sliders.click()
-
-        move_sliders.move_to_element_with_offset(
-            to_element=contrast_slider,
-            xoffset=1,
-            yoffset=contrast_slider_size['height'] / 2,
-        )
-        move_sliders.click()
-        move_sliders.perform()
-
-        filter_style = image_view_port_element.value_of_css_property('filter')
-        self.assertEqual('invert(0) contrast(0) brightness(0)', filter_style)
-
-        close_image_editor_button = driver.find_element_by_id(
-            'id_close_image_editor_button',
-        )
-        close_image_editor_button.click()
-
-        filter_style = image_view_port_element.value_of_css_property('filter')
-        self.assertEqual('invert(0) contrast(2) brightness(2)', filter_style)
 
         expand_settings_elem.click()
         wait = WebDriverWait(driver, timeout=5)
@@ -358,6 +360,101 @@ class TestNormalFlow(TestCase):
 
         wait.until(extra_image_settings_not_displayed)
 
+    def _test_invert(
+            self,
+            driver: WebDriver,
+            image_view_port_elem: WebElement,
+    ) -> None:
+        invert_button = driver.find_element_by_id('id_invert_button')
+        invert_button.click()
+
+        filter_style = image_view_port_elem.value_of_css_property('filter')
+
+        self.assertEqual('invert(1) contrast(1) brightness(1)', filter_style)
+
+        invert_button.click()
+
+        filter_style = image_view_port_elem.value_of_css_property('filter')
+
+        self.assertEqual('invert(0) contrast(1) brightness(1)', filter_style)
+
+    def _test_apply_modal(
+            self,
+            brightness_slider: WebElement,
+            contrast_slider: WebElement,
+            driver: WebDriver,
+            image_view_port_elem: WebElement,
+    ) -> None:
+        move_sliders = ActionChains(driver)
+        brightness_slider_size = brightness_slider.size
+        contrast_slider_size = contrast_slider.size
+        move_sliders.move_to_element_with_offset(
+            to_element=brightness_slider,
+            xoffset=brightness_slider_size['width'] - 1,
+            yoffset=brightness_slider_size['height'] / 2,
+        )
+        move_sliders.click()
+
+        move_sliders.move_to_element_with_offset(
+            to_element=contrast_slider,
+            xoffset=contrast_slider_size['width'] - 1,
+            yoffset=contrast_slider_size['height'] / 2,
+        )
+        move_sliders.click()
+
+        move_sliders.perform()
+        apply_button = driver.find_element_by_id(
+            'id_apply_image_editor_button',
+        )
+        apply_button.click()
+
+        filter_style = image_view_port_elem.value_of_css_property('filter')
+
+        self.assertEqual('invert(0) contrast(2) brightness(2)', filter_style)
+
+    def _test_cancel_modal(
+            self,
+            brightness_slider: WebElement,
+            contrast_slider: WebElement,
+            driver: WebDriver,
+            image_view_port_elem: WebElement,
+    ) -> None:
+        brightness_slider_size = brightness_slider.size
+        contrast_slider_size = contrast_slider.size
+        move_sliders = ActionChains(driver)
+        move_sliders.move_to_element_with_offset(
+            to_element=brightness_slider,
+            xoffset=1,
+            yoffset=brightness_slider_size['height'] / 2,
+        )
+        move_sliders.click()
+
+        move_sliders.move_to_element_with_offset(
+            to_element=contrast_slider,
+            xoffset=1,
+            yoffset=contrast_slider_size['height'] / 2,
+        )
+        move_sliders.click()
+
+        move_sliders.perform()
+
+        filter_style = image_view_port_elem.value_of_css_property('filter')
+
+        self.assertEqual('invert(0) contrast(0) brightness(0)', filter_style)
+
+        close_image_editor_button = driver.find_element_by_id(
+            'id_close_image_editor_button',
+        )
+        close_image_editor_button.click()
+
+        filter_style = image_view_port_elem.value_of_css_property('filter')
+
+        self.assertEqual('invert(0) contrast(2) brightness(2)', filter_style)
+
+    def _test_thumbnails(
+            self,
+            driver: WebDriver,
+    ) -> None:
         image_1_elem = driver.find_element_by_id('id_image_1')
         image_2_elem = driver.find_element_by_id('id_image_2')
         self.assertTrue(image_1_elem.is_displayed())
@@ -371,10 +468,3 @@ class TestNormalFlow(TestCase):
         thumbnail_2_elem.click()
         self.assertFalse(image_1_elem.is_displayed())
         self.assertTrue(image_2_elem.is_displayed())
-        json_coverage = driver.execute_script(
-            'return JSON.stringify(window.__coverage__);',
-        )
-        with open(
-                f'/opt/friendexing/tests/coverage_{coverage_name}.json', 'w'
-        ) as json_file:
-            json_file.write(json_coverage)
